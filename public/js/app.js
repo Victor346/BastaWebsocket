c31c8cd405a39e947a3bae3af386f2a39552776c
Victor Lopez <A01336434@itesm.mx>
2020-11-12 17:58:55 -0600

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+function makeToast() {
+  $.toast({
+    text: 'This is called a toast!',
+    position: 'top-right'
+  });
+}
+
+function makeToastMessage(message) {
+  $.toast({
+    text: message,
+    position: 'top-right'
+  });
+}
+
+window.socket = null;
+function connectToSocketIo() {
+  let server = window.location.protocol + "//" + window.location.host;
+  window.socket = io.connect(server);
+  // Recibe un mensaje de tipo toast
+  window.socket.on('toast', function (data) {
+    // Muestra el mensaje
+    makeToastMessage(data.message);
+  });
+}
+
+function emitEventToSocketIo() {
+  let text = $('#messageToServer').val();
+  // Envía un mensaje
+  window.socket.emit('messageToServer', { text: text });
+}
+
+$(function () {
+  connectToSocketIo();
+});
